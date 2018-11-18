590d31e41805f9c2c663246a677918b6b53d6c47
Vladimir Feinberg <vladimir.feinberg@gmail.com>
2018-11-18 13:57:54 -0800

    add latex compilation steps

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 if ! which pdflatex 2> /dev/null ; then
-  sudo apt-get -y install texlive-latex-base texlive-fonts-recommended  texlive-fonts-extra
+    if which apt-get ; then
+        echo installing pdflatex with sudo
+        sudo apt-get -y install texlive-latex-base texlive-fonts-recommended  texlive-fonts-extra
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        echo josh figure this out
+    else
+        echo "dunno how to install pdflatex here, you figure it out"
+    fi
 fi
 
 
@@ -10,7 +17,7 @@ for i in *.tex ; do
     echo compiling $i
     ARGUMENT="$i"
     AUXNAME="${ARGUMENT%.tex}.aux"
-    pdflatex $i # -shell-escape -interaction=nonstopmode -file-line-error "$ARGUMENT" | grep -i ".*:[0-9]*:.*\|warning" 
+    pdflatex -shell-escape -interaction=nonstopmode -file-line-error "$ARGUMENT" | grep -i ".*:[0-9]*:.*\|warning" 
     # bibtex -terse "$AUXNAME"  
     pdflatex -shell-escape -interaction=nonstopmode -file-line-error "$ARGUMENT" | grep -i ".*:[0-9]*:.*\|warning" 
 done
